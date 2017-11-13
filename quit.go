65dcc9c95b78bc12e473f05d77cb114c9ea7c176
thelark <mperial@foxmail.com>
2017-11-13 16:47:00 +0800

    init
    project quit init

---
+++ quit.go
@@ -0,0 +1,5 @@
+package quit
+
+func SafeExit() {
+	safeExit()
+}
---
+++ quit_windows.go
@@ -0,0 +1,28 @@
+// +build windows
+
+package quit
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+	"fmt"
+)
+
+func safeExit() {
+	// 创建监听退出chan
+	s := make(chan os.Signal)
+	//监听指定信号 ctrl+c kill
+	signal.Notify(s, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		for c := range s {
+			switch c {
+			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+				fmt.Println("退出", c)
+				os.Exit(0)
+			default:
+				fmt.Println("other", c)
+			}
+		}
+	}()
+}
